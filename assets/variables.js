1a037895f0e9dd0b0ec9ae512c2114dcd5564cf0
basstimam <64816240+basstimam@users.noreply.github.com>
2020-05-05 07:44:10 +0700

    Add files via upload

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["KAMUU", "KAMUUU", "KAMUUUU", "KAMUUUUU", "KAMUUUUUU"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
