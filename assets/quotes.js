7555dc6fbdfcf03f45c8592f80468040bf70a43b
basstimam <64816240+basstimam@users.noreply.github.com>
2020-05-05 20:45:02 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,5 @@
 // prettier-ignore
 export default [
-    { author: "imam", quotes: "I love you, Will u be my gf?" },
-    { author: "imam", quotes: "I love you, Will u be my gf?" },
-    { author: "imam", quotes: "I love you, Will u be my gf?" },
-    { author: "imam", quotes: "I love you, Will u be my gf?" },
-    { author: "imam", quotes: "I love you, Will u be my gf?" },
-    { author: "imam", quotes: "I love you, Will u be my gf?" },
-    { author: "imam", quotes: "I love you, Will u be my gf?" },
+    { author: "imam", quotes: "Whatever happens, keep breathing" },
+  
 ];
