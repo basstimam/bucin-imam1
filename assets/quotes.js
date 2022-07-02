c69c1104595132c8fa66027471e4624d4f00bc7b
re-ten <64816240+basstimam@users.noreply.github.com>
2022-07-02 14:37:10 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,5 +1,5 @@
 // prettier-ignore
 export default [
-    { author: "imam", quotes: "Whatever happens, keep breathing" },
+    { author: "imam", quotes: "I always thinkin' about u every morning" },
   
 ];
